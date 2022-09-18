afe3083a0d4a1d88fe42e47a8ae0c07962db4389
Sean Yang <hewas6@gmail.com>
2022-09-18 09:41:16 -0400

    custom css on tailwind, add 7 icons on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
 import {
-  BeakerIcon,
   ChevronDownIcon,
   HomeIcon,
+  MagnifyingGlassIcon,
 } from '@heroicons/react/24/solid';
-import { StarIcon } from '@heroicons/react/24/outline';
+import {
+  BellIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  GlobeAltIcon,
+  PlusIcon,
+  SparklesIcon,
+  MegaphoneIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/24/outline';
 
 function Header() {
   return (
@@ -23,6 +31,31 @@ function Header() {
         <HomeIcon className="h-5 w-5" />
         <p className="flex-1 ml-2 hidden lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* search box */}
+      <form
+        action=""
+        className="flex flex-1 items-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1"
+      >
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div className="mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeAltIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatBubbleOvalLeftEllipsisIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <MegaphoneIcon className="icon" />
       </div>
     </div>
   );
