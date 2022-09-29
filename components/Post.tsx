8fbf4427152fa358b7128a9ec9a8427b948482bb
Sean Yang <hewas6@gmail.com>
2022-09-29 12:53:34 -0400

    add react-timeago

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,12 +1,14 @@
-import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/24/outline';
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/24/outline'
+import Avatar from './Avatar'
+import TimeAgo from 'react-timeago'
 
 type Props = {
-  post: Post;
-};
+  post: Post
+}
 
-export const Post = (post: Props) => {
+export const Post = ({ post }: Props) => {
   return (
-    <div>
+    <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border hover:border-gray-600">
       {/* Votes */}
       <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
         <ArrowUpIcon className="voteButtons hover:text-red-400" />
@@ -14,16 +16,29 @@ export const Post = (post: Props) => {
         <ArrowDownIcon className="voteButtons hover:text-blue-400" />
       </div>
 
-      <div>
+      <div className="p-3 pb-1">
         {/* Header */}
-        <div></div>
+        <div className="flex items-center space-x-2">
+          <Avatar seed={post.subreddit[0]?.topic} />
+          <p className="text-xs text-gray-400">
+            <span className="font-bold text-black hover:text-blue-400 hover:underline">
+              r/{post.subreddit[0]?.topic}
+            </span>{' '}
+            - Posted by u/
+            {post.username} <TimeAgo date={post.created_at} />
+          </p>
+        </div>
 
         {/* Body */}
+        <div className="py-4">
+          <h2 className="text-xl font-semibold">{post.title}</h2>
+          <p className="mt-2 text-sm font-light">{post.body}</p>
+        </div>
 
         {/* Image */}
 
         {/* Footer */}
       </div>
     </div>
-  );
-};
+  )
+}
